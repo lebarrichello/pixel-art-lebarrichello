19528eb3a2793e5b6e2abb5e59f4c110b2c91f12
lebarrichello <leticia.barrichello@gmail.com>
2023-02-09 15:54:56 -0300

    requisito 4

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
 let penColour = 'white';
 
-// eslint-disable-next-line no-unused-vars
 function setPenColour(pen) {
-  penColour = pen;
+  penColour = pen.style.backgroundColor;
 }
 
-// eslint-disable-next-line no-unused-vars
 function setPixelColour(pixel) {
-  // eslint-disable-next-line no-param-reassign
   pixel.style.backgroundColor = penColour;
+}
+
+function coresAleatorias() {
+  const hex = (Math.random() * 0xFFFFFF << 0).toString(16);
+  return `#${hex}`;
+}
+console.log(coresAleatorias());
+
+function btnGeraCor() {
+  const cores = document.querySelectorAll('.color');
+
+  cores.forEach((e) => {
+   if (e.style.backgroundColor != 'black') {
+    const novaCor = coresAleatorias();
+    e.style.backgroundColor = novaCor;
+  }});
 }
