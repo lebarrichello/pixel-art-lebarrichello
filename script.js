ee3121e98dcc00958ef395dcfb3a28e9b96c36f8
lebarrichello <leticia.barrichello@gmail.com>
2023-02-13 21:53:59 -0300

    requisitos ok 1-7

--- script.js
+++ script.js
@@ -1,25 +1,109 @@
-let penColour = 'white';
+
+ let penColour = 'black';
 
 function setPenColour(pen) {
   penColour = pen.style.backgroundColor;
+ 
 }
 
 function setPixelColour(pixel) {
   pixel.style.backgroundColor = penColour;
-}
+ 
+} 
 
 function coresAleatorias() {
   const hex = (Math.random() * 0xFFFFFF << 0).toString(16);
   return `#${hex}`;
 }
-console.log(coresAleatorias());
+
+function iniciaCor() {
+    let cores = document.querySelectorAll('.color');
+    colorPalette = JSON.parse(localStorage.getItem('colorPalette'));
+    let novas_cores = []
+    
+    if (colorPalette === null){
+    cores.forEach((e) => {
+      if (e.style.backgroundColor === '#FFFFFF') {
+        const novaCor = coresAleatorias();
+        e.style.backgroundColor = novaCor;
+      }
+      if ((e.style.backgroundColor === e.style.backgroundColor) && (e.style.backgroundColor != 'black')) {
+        const novaCor = coresAleatorias();
+        e.style.backgroundColor = novaCor;
+        novas_cores.push(novaCor)
+      }
+         })
+    localStorage.setItem('colorPalette', JSON.stringify(novas_cores));
+    } else{
+
+        lista_sem_black = Array.from(cores).filter((e) => e.style.backgroundColor != 'black');
+        lista_sem_black.forEach((e, i) => {
+            e.style.backgroundColor = colorPalette[i];} )
+    }
+}
 
 function btnGeraCor() {
-  const cores = document.querySelectorAll('.color');
-
-  cores.forEach((e) => {
-   if (e.style.backgroundColor != 'black') {
-    const novaCor = coresAleatorias();
-    e.style.backgroundColor = novaCor;
-  }});
+    const cores = document.querySelectorAll('.color');
+    let novas_cores = []
+    cores.forEach((e) => {
+      if (e.style.backgroundColor === '#FFFFFF') {
+        const novaCor = coresAleatorias();
+        e.style.backgroundColor = novaCor;
+        novas_cores.push(novaCor);
+      }
+      if ((e.style.backgroundColor === e.style.backgroundColor) && (e.style.backgroundColor != 'black')) {
+        const novaCor = coresAleatorias();
+        e.style.backgroundColor = novaCor;
+        novas_cores.push(novaCor);
+      }
+         });
+    localStorage.setItem('colorPalette', JSON.stringify(novas_cores));
+    console.log(localStorage.getItem('colorPalette'));
 }
+
+
+function btnLimpaCor() {
+    const limpaPixel = document.querySelectorAll('.pixel');
+    limpaPixel.forEach((e) => {
+      e.style.backgroundColor = 'white';
+    });
+    localStorage.removeItem('pixelBoard');
+  }
+
+const pixels = document.querySelectorAll('.pixel');
+document.addEventListener('click', (evento) => {
+  const corPixel = [];
+  for (let i = 0; i < pixels.length; i += 1) {
+    if (evento.target.classList.contains('pixel')) {
+      corPixel.push(pixels[i].style.backgroundColor);
+    } else {
+      return corPixel;
+    }
+  }
+  localStorage.setItem('pixelBoard', JSON.stringify(corPixel));
+});
+
+const salvarDesenho = () => {
+  const desenho = JSON.parse(localStorage.getItem('pixelBoard'));
+  if (desenho === null) {
+    return;
+  }
+  const pixelQuadro = document.querySelectorAll('.pixel');
+  for (let i = 0; i < pixels.length; i += 1) {
+    pixelQuadro[i].style.backgroundColor = desenho[i];
+  }
+}
+
+
+/* const salvarPaleta = () => {
+    const paleta = JSON.parse(localStorage.getItem('colorPalette'));
+    if (paleta === null) {
+        return;
+    }
+} */
+
+window.onload = () => {
+  salvarDesenho();
+  iniciaCor();
+ /*  salvarPaleta(); */
+};
